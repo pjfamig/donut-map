72fa6616478c05d61143ed80486c326ab09da650
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-01-25 21:13:31 -0800

    Ditch middleware again

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,5 @@ module Donutmap
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.middleware.insert_before 0, "SearchSuggestions"
-    
   end
 end
