5888dcf518c223f16a49947592e7233ad10b5def
Paul Famiglietti <p0seid0n_@hotmail.com>
2014-01-26 12:57:56 -0800

    Readded middleware after connection adapters

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ Bundler.require(:default, Rails.env)
 
 module Donutmap
   class Application < Rails::Application
+    config.middleware.insert_after 12, "SearchSuggestions"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
